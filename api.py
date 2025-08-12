751e1b52961f6df38474f08d6a221996cb6de62f
Kaique Nascimento <kaiquepalmanascimento@hotmail.com>
2025-08-12 16:07:31 -0300

    v2

--- api.py
+++ api.py
@@ -1,25 +1,26 @@
-from fastapi import FastAPI
-from random import randint
+from flask import Flask, jsonify
+import random
 
-app = FastAPI()
+app = Flask(__name__)
 
 piadas = {
-    1: {"piada" : "Sua mãe é tão gorda, mas tão gorda, que ela precisa comprar roupa na 'VestCasa'"},
-    2: {"piada" : "Sua mãe é tão burra, mas tão burra, que ela roubou amostra grátis"},
-    3: {"piada" : "Hoje na nossa loja temos Promoção, Promocinho e Provizinho!"},
-    4: {"piada" : "Eu sou muito bom em engenharia quântica, química e em contar mentiras"},
+    1: {"piada": "Sua mãe é tão gorda, mas tão gorda, que ela precisa comprar roupa na 'VestCasa'"},
+    2: {"piada": "Sua mãe é tão burra, mas tão burra, que ela roubou amostra grátis"},
+    3: {"piada": "Hoje na nossa loja temos Promoção, Promocinho e Provizinho!"},
+    4: {"piada": "Eu sou muito bom em engenharia quântica, química e em contar mentiras"},
 }
 
-@app.get("/")
-async def root():
-    randomico = randint(1, len(piadas))
-    return {"piadaAleatoria" : piadas[randomico]}
+@app.route("/")
+def root():
+    randomico = random.randint(1, len(piadas))
+    return jsonify({"piadaAleatoria": piadas[randomico]})
 
-
-@app.get("/piadas/{id_piada}")
-def pegar_piada(id_piada:int):
+@app.route("/piadas/<int:id_piada>")
+def pegar_piada(id_piada):
     if id_piada in piadas:
-
-        return piadas[id_piada]
+        return jsonify(piadas[id_piada])
     else:
-        return {"Erro":"Id de piada inexistente"}
+        return jsonify({"Erro": "Id de piada inexistente"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
